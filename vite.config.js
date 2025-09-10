8c283f8026acb98613427c17f3167c99f42f019b
Nitish Mandal <nitishmandal367@gmail.com>
2025-09-10 21:50:14 +0530

    Final commit for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/odms_project/', // Match your repository name
   plugins: [react()],
-  server: {
-    host: true,  // exposes server to your LAN
-    port: 5173,  // optional, default is 5173
-  },
-})
+});
